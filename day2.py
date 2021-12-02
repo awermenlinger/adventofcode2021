b14156bbf7e06d1089c8fa9435efe5b55bd6c2a8
Antoine W <a.wermenlinger@gmail.com>
2021-12-02 15:41:43 +0100

    day2 complete

---
+++ day2.py
@@ -0,0 +1,26 @@
+horizontal = 0
+depth = 0
+
+moves = open('files\day2.txt', 'r').read().rstrip().split('\n')
+
+for move in moves:
+   if move[0] == "f": horizontal += int(move[-1])
+   elif move[0] == "u": depth -= int(move[-1])
+   elif move[0] == "d": depth += int(move[-1])
+
+print("1 - Forward: {}  Depth: {}   Result: {}".format(horizontal, depth, horizontal*depth))
+
+horizontal = 0
+depth = 0
+aim = 0
+
+moves = open('files\day2.txt', 'r').read().rstrip().split('\n')
+
+for move in moves:
+   if move[0] == "f": 
+       horizontal += int(move[-1])
+       depth += aim * int(move[-1])
+   elif move[0] == "u": aim -= int(move[-1])
+   elif move[0] == "d": aim += int(move[-1])
+
+print("1 - Forward: {}  Depth: {}   Result: {}".format(horizontal, depth, horizontal*depth))
